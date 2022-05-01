ace4f21244d03cb79703171930204ba725c82cab
hakob <hakob21@gmail.com>
2022-05-02 00:20:46 +0200

    WIP button setOnAction without lambda added

--- src/main/kotlin/com/hakob/flashcards/testPack/JavaFxAppStarter.kt
+++ src/main/kotlin/com/hakob/flashcards/testPack/JavaFxAppStarter.kt
@@ -2,12 +2,25 @@ package com.hakob.flashcards.testPack
 
 import com.hakob.flashcards.utils.SceneUtils
 import com.hakob.flashcards.utils.TranslateUtils
-import javafx.scene.control.Hyperlink
+import com.sun.javafx.geom.Rectangle
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.Group
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.*
+import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
 import javafx.scene.text.TextFlow
+import javafx.scene.web.HTMLEditor
 import javafx.stage.Stage
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.io.File
+
 
 @Service
 class JavaFxAppStarter {
@@ -22,14 +35,65 @@ class JavaFxAppStarter {
     lateinit var sceneUtils: SceneUtils
 
     fun runApp(stage: Stage) {
-        val listOfWords = text.split(" ", "?<=,", "?<=.")
-        val hyperLinkWordList = listOfWords.map { Hyperlink(it) }.toList()
+        stage.title = "HTMLEditor Sample"
+        stage.width = 650.0
+        stage.height = 500.0
+        val scene = Scene(Group())
+        var root = VBox()
+        root.padding = Insets(8.0, 8.0, 8.0, 8.0)
+        root.spacing = 5.0
+        root.alignment = Pos.BOTTOM_LEFT
+        val htmlEditor = HTMLEditor()
+        htmlEditor.prefHeight = 245.0
+        htmlEditor.htmlText = "textTest"
+        val htmlCode = TextArea()
+        htmlCode.setWrapText(true)
+        val scrollPane = ScrollPane()
+        scrollPane.getStyleClass().add("noborder-scroll-pane")
+        scrollPane.setContent(htmlCode)
+        scrollPane.setFitToWidth(true)
+        scrollPane.setPrefHeight(180.0)
+        val showHTMLButton = Button("Produce HTML Code")
+        root.alignment = Pos.CENTER
+        //button
+//        showHTMLButton.setOnAction { arg0: ActionEvent? -> htmlCode.setText(htmlEditoor.htmlText) }
+        showHTMLButton.setOnAction(object : EventHandler<ActionEvent?> {
+            override fun handle(event: ActionEvent?) {
+                val secondLabel = Label("I'm a Label on new Window")
+                val secondaryLayout = StackPane()
+                secondaryLayout.children.add(secondLabel)
+                val secondScene = Scene(secondaryLayout, 230.0, 100.0)
 
+                // New window (Stage)
+                val newWindow = Stage()
+                newWindow.title = "Second Stage"
+                newWindow.scene = secondScene
+
+                // Set position of second window, related to primary window.
+                newWindow.x = stage.getX() + 200
+                newWindow.y = stage.getY() + 100
+                newWindow.show()
+            }
+        })
+
+        root.children.addAll(htmlEditor, showHTMLButton, scrollPane)
+        scene.root = root
+        stage.scene = scene
+        stage.show()
+
+
+
+
+
+
+//        val listOfWords = text.split(" ", "?<=,", "?<=.")
+//        val hyperLinkWordList = listOfWords.map { Hyperlink(it) }.toList()
+//
         // create TextFlow
-        val textFlow = createTextFlow(hyperLinkWordList, listOfWords)
+//        val textFlow = createTextFlow(hyperLinkWordList, listOfWords)
 
         // create a scene
-        sceneUtils.createScene(textFlow, stage)
+//        sceneUtils.createScene(textFlow, stage)
     }
 
     private fun createTextFlow(
